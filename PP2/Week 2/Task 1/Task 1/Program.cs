ffacf943cc20024f943b442ffb3629abd68625b2
Bagdaulet881 <43570028+Bagdaulet881@users.noreply.github.com>
2019-02-15 08:11:44 +0600

    Add files via upload

---
+++ PP2/Week 2/Task 1/Task 1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+
+namespace ReadFromFile
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            int cnt = 0;
+            FileStream fileStream = new FileStream(@"C:\csh\PP2\Week 2\Task 1\Task 1\input.txt", FileMode.Open, FileAccess.Read);
+            StreamReader sr = new StreamReader(fileStream);
+
+            String str = sr.ReadLine();
+
+
+            
+            
+                int i = 0;
+                int j = str.Length - 1;
+
+                while (i < j)
+                {
+                if (str[i] != str[j])
+                {
+                    cnt++;
+                    i++;
+                    j--;
+                }
+                else
+                {
+                    i++;
+                    j--;
+                }
+                
+                }
+            if (cnt > 0)
+            {
+                Console.WriteLine("No");
+            }
+            else
+            {
+                Console.WriteLine("Yes");
+            }
+
+                   
+            sr.Close();
+            fileStream.Close();
+
+        }
+    }
+}
---
+++ PP2/Week 2/Task 4/Task 4/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+
+class Test
+{
+    public static void Main()
+    {
+        string file = "input.txt";
+        string path = @"C:\csh\PP2\Week 2\Task 4\Task 4\path\" + file;
+        string path1 = @"C:\csh\PP2\Week 2\Task 4\Task 4\path1\" + file;
+        
+        //CREATE
+        FileStream fs = new FileStream(path, FileMode.Create, FileAccess.Write);
+        //COPY
+        fs.Close();
+        File.Copy(path,path1);
+        //DELETE
+        File.Delete(path);
+    }
+}
