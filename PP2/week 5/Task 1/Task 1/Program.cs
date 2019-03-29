7dd9a54fbdefae783ffb56a8d36141c9a5e74ebf
Bagdaulet881 <43570028+Bagdaulet881@users.noreply.github.com>
2019-03-29 07:23:12 +0600

    Add files via upload

---
+++ PP2/week 5/Task 1/Task 1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Task_1
+{
+    public class ComplexNumbers
+    {
+        public int a;
+        public int b;
+        public ComplexNumbers()
+        {
+
+        }
+        public void PrintInfo()
+        {
+            Console.WriteLine(a + " + " + b + "i");
+        }
+    }
+
+    class Program
+    {
+        private static void Serialize()
+        {
+            ComplexNumbers cmplx = new ComplexNumbers();
+            cmplx.a = Convert.ToInt32(Console.ReadLine());
+            cmplx.b = Convert.ToInt32(Console.ReadLine());
+            FileStream fs = new FileStream("complex.xml", FileMode.OpenOrCreate, FileAccess.Write);
+            XmlSerializer xs = new XmlSerializer(typeof(ComplexNumbers));
+            xs.Serialize(fs, cmplx);
+            fs.Close();
+        }
+        private static void DeSerialize()
+        {
+            FileStream fss = new FileStream("complex.xml", FileMode.Open, FileAccess.Read);
+            XmlSerializer xs = new XmlSerializer(typeof(ComplexNumbers));
+            ComplexNumbers cmplx = xs.Deserialize(fss) as ComplexNumbers;
+            cmplx.PrintInfo();
+            fss.Close();
+        }
+            static void Main(string[] args)
+        {
+            Serialize();
+            DeSerialize();
+        }
+    }
+}
---
+++ PP2/week 5/Task 2/Task 2/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Task_2
+{
+    public class MarkList   
+    {
+        public List<Mark> Marks;
+        public MarkList()
+        {
+            Marks = new List<Mark>();
+        }
+
+        public void Serialize(MarkList MRK)
+        {
+            FileStream fs = new FileStream("marks.xml", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            XmlSerializer xs = new XmlSerializer(typeof(MarkList));
+            xs.Serialize(fs, MRK);
+            fs.Close();
+        }
+        public void DeSerialize()
+        {
+            FileStream fs = new FileStream("marks.xml", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            XmlSerializer xs = new XmlSerializer(typeof(MarkList));
+            MarkList MRK = xs.Deserialize(fs) as MarkList;
+           for (int i = 0; i < MRK.Marks.Count; i++)
+            {
+                Console.WriteLine(MRK.Marks[i]);
+            }
+            fs.Close();
+        }
+    }
+    public class Mark
+    {
+        public int Points;
+
+        public Mark() { }
+        public Mark(int Points)
+        {
+            this.Points = Points;
+        }
+
+        public string GetLetter()
+        {
+            if (Points >= 95)
+                return "A";
+            if (Points >= 90 && Points < 95)
+                return "A-";
+            if (Points >= 85 && Points < 90)
+                return "B+";
+            if (Points >= 80 && Points < 85)
+                return "B";
+            if (Points >= 75 && Points < 80)
+                return "B-";
+            if (Points >= 70 && Points < 75)
+                return "C+";
+            if (Points >= 65 && Points < 70)
+                return "C";
+            if (Points >= 60 && Points < 65)
+                return "C-";
+            if (Points >= 55 && Points < 60)
+                return "D+";
+            if (Points >= 50 && Points < 55)
+                return "D";
+            return "F";
+        }
+        public override string ToString()
+        {
+            return GetLetter();
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mark m1 = new Mark(95);
+            Mark m2 = new Mark(85);
+            Mark m3 = new Mark(57);
+            MarkList MRK = new MarkList();
+            MRK.Marks.Add(m1);
+            MRK.Marks.Add(m2);
+            MRK.Marks.Add(m3);
+            MRK.Serialize(MRK);
+            MRK.DeSerialize();
+            Console.ReadKey();
+        }
+    }
+}
